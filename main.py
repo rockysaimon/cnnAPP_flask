96cc852694e80804b142a073defe2c8f24749d93
Simón Zapata Florez <Zapataflorezsimon623@gmail.com>
2024-11-14 16:36:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import tensorflow as tf
 import pefile
 import cv2
 import matplotlib.pyplot as plt
-
+'''
+Importa librerías necesarias. Flask para la app web, os para manejar archivos, numpy para manejar datos en arrays,
+tensorflow para cargar el modelo de IA, pefile para analizar archivos ejecutables, cv2 no se utiliza
+y matplotlib para guardar imágenes como archivos PNG.
+'''
 app = Flask(__name__)
 model = tf.keras.models.load_model('train_model/modelo3.keras')  # Reemplaza con la ruta a tu modelo
+'''Crea la instancia de FLASK y luego carga el modelo entrenado'''
 
 @app.route('/')
 def index():
     return render_template('index.html')
+'''Define la ruta principal / que carga la página HTML index.html cuando se ejecuta'''
 
 @app.route('/select_file', methods=['POST'])
 def select_file():
@@ -24,7 +30,8 @@ def select_file():
     # Procesar el archivo
     file_path = os.path.join('uploads', file.filename)
     file.save(file_path)
-
+'''Define una ruta para recibir archivos /select_file. Verifica que el archivo 
+esté presente en la solicitud (request.files). Si hay un archivo, lo guarda en la carpeta uploads (se crea automático)'''
     try:
         binary_image = convert_to_binary_image(file_path)
         predicted_class, probability = predict_malware(binary_image)
@@ -43,6 +50,9 @@ def select_file():
         })
     except Exception as e:
         return jsonify({'error': str(e)})
+    '''Convierte el archivo a una imagen binaria usando la función convert_to_binary_image, predice si es malware
+    con predict_malware, guarda la imagen en la ruta 'static/binary_image.png' y muestra el resultado. 
+    Si ocurre un error, lo devuelve en formato JSON (ya que este se recibe en el javascript para que se muestre en pantalla)'''
 
 def convert_to_binary_image(file_path):
     pe = pefile.PE(file_path)
@@ -64,11 +74,16 @@ def convert_to_binary_image(file_path):
     rgb_image = np.repeat(np.expand_dims(binary_image, axis=-1), 3, axis=-1)
     return rgb_image
 
+''' Convierte el archivo PE (Portable Executable) o exe en una imagen binaria en escala de grises de dimensiones 256x256. 
+Usa solo ciertas secciones (.data, .rsrc, .rdata, .text) (estas secciones las explico yo a fondo) y ajusta la imagen para 
+que tenga el tamaño adecuado.'''
+
 def predict_malware(binary_image):
     predictions = model.predict(np.expand_dims(binary_image, axis=0))
     predicted_class = np.argmax(predictions)
     probability = predictions[0][predicted_class]
     return predicted_class, probability
+    '''Usa el modelo cargado para predecir la clase del malware y devuelve la clase más probable junto con su probabilidad.'''
 
 def show_prediction(class_index, probability):
     classes = [
@@ -83,8 +98,14 @@ def show_prediction(class_index, probability):
     class_name = classes[class_index]
     print(f'Predicted Class: {class_name}, Probability: {probability}')
     return class_name
+    '''mapea el índice de clase predicho (predicted_class) a la familia de malware. 
+    Imprime el resultado y devuelve el nombre de la clase.
+    es decir class_index que es predicted_class que viene de la función predict_malware,
+    es un número (índice) que es la posición en el arreglo de la familia a la que pertenece'''
 
 if __name__ == '__main__':
     os.makedirs('uploads', exist_ok=True)
     os.makedirs('static', exist_ok=True)
     app.run(debug=True)
+
+'''Asegura que las carpetas uploads y static existen para iniciar la aplicación'''
